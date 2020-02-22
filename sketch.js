44842f54d5fc31daef413c6f63f542ebc7210f9f
Rajbir008 <57822628+Rajbir008@users.noreply.github.com>
2020-02-22 09:58:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas,backgroundImage; 
+var gameState = 0;
+var playerCount;
+var database;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  
+}
+
+function draw(){
+ 
+}
+
